ae805387b3ff9acf791ba184cf050ca961fc936f
MDSONU22943 <mdsonu22943@gmail.com>
2025-11-17 20:37:07 +0530

    seventh commit

---
+++ 17_interfaces/interfaces.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type payment struct {
+	gateway stripe
+}
+
+func (p payment) makePayment(amount float32) {
+	// razorpayPaymentGw := razorpay{}
+	// razorpayPaymentGw.pay(amount)
+
+	// stripePaymentGw := stripe{}
+	// stripePaymentGw.pay(amount)
+
+	p.gateway.pay(amount)
+}
+
+type razorpay struct{}
+
+func (r razorpay) pay(amount float32) {
+
+	// logic to make payment
+	fmt.Println("making payment using razorpay", amount)
+}
+
+type stripe struct{}
+
+func (s stripe) pay(amount float32){
+	fmt.Println("making payment using strip", amount)
+}
+
+func main() {
+	newPayment := payment{}
+	newPayment.makePayment(100)
+}
---
+++ 18_enums/enums.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// type OrderStatus int
+type OrderStatus string
+
+const (
+	Received  OrderStatus = "received"
+	Confirmed             = "confirmed"
+	Prepared              = "prepared"
+	Delivered             = "delivered"
+)
+
+func changeOrderStatus(status OrderStatus) {
+	fmt.Println("changing order status to", status)
+}
+
+func main() {
+	changeOrderStatus(Delivered)
+
+}
